f3eb012fa3b87974b00026aea833cf30e2646a4e
Isaac Abraham <isaac.abraham@gmail.com>
2017-12-03 13:11:48 +0100

    Create dev project and slimmed down dependencies for ARM Helper.

--- src/FsHelpers/ArmHelper/ArmHelper.fs
+++ src/FsHelpers/ArmHelper/ArmHelper.fs
@@ -1,9 +1,7 @@
 module Cit.Helpers.Arm
 
-open Microsoft.Azure.Management.Fluent
 open Microsoft.Azure.Management.ResourceManager.Fluent
 open Microsoft.Azure.Management.ResourceManager.Fluent.Authentication
-open Microsoft.Azure.Management.ResourceManager.Fluent.Deployment.Definition
 open Newtonsoft.Json
 open Newtonsoft.Json.Linq
 open System
@@ -12,7 +10,7 @@ type ParameterValue = { value : string } static member Create value = { value = 
 type OutputResult = { Type : string; Value : string }
 type DeploymentOutputs = Map<string, string>
 type AuthenticationCredentials = { ClientId : Guid; ClientSecret : string; TenantId : Guid }
-type DeploymentStatus = DeploymentInProgress of state:string * operations:int | DeploymentError of statusCode:string * message:string | DeploymentCompleted of deployment:IDeployment * DeploymentOutputs
+type DeploymentStatus = DeploymentInProgress of state:string * operations:int | DeploymentError of statusCode:string * message:string | DeploymentCompleted of deployment:DeploymentOutputs
 
 let private (|Accepted|Running|Succeeded|Failed|Other|) = function
     | "Accepted" -> Accepted
@@ -24,8 +22,7 @@ let private (|Accepted|Running|Succeeded|Failed|Other|) = function
 /// Authenticates to Azure using the supplied credentials for a specific subscription.
 let authenticateToAzure credentials (subscriptionId:Guid) =
     let spi = AzureCredentialsFactory().FromServicePrincipal(string credentials.ClientId, credentials.ClientSecret, string credentials.TenantId, AzureEnvironment.AzureGlobalCloud)
-    Azure
-        .Configure()
+    ResourceManager
         .Authenticate(spi)
         .WithSubscription(string subscriptionId)
 
@@ -37,15 +34,15 @@ let private toDeploymentOutputs : obj -> DeploymentOutputs = function
         |> Map.map(fun _ v -> v.Value)
     | _ -> failwith "Unknown output type!"
 
-/// Deploys an ARM template, providing a stream of progress updates and culminating with the deployment and any outputs.
-let rec deployTemplate (deployment:IDeployment) = seq {
+/// Deploys an ARM template, providing a stream of progress updates and culminating with any outputs.
+let rec deployTemplateWithStatus (deployment:IDeployment) = seq {
     let deployment = deployment.Refresh()
     let operations = deployment.DeploymentOperations.List() |> Seq.toArray
     yield DeploymentInProgress(deployment.ProvisioningState, operations.Length)
     match deployment.ProvisioningState with
     | Running | Accepted | Other _ ->            
         Async.Sleep 5000 |> Async.RunSynchronously
-        yield! deployTemplate deployment
+        yield! deployTemplateWithStatus deployment
     | Failed ->
         yield!
             operations
@@ -55,7 +52,13 @@ let rec deployTemplate (deployment:IDeployment) = seq {
                 | _ -> None)
             |> Seq.map DeploymentError
         failwith "Failed to complete deployment successfully."
-    | Succeeded -> yield DeploymentCompleted (deployment, deployment.Outputs |> toDeploymentOutputs) }
+    | Succeeded -> yield DeploymentCompleted (deployment.Outputs |> toDeploymentOutputs) }
+
+/// Deploys an ARM template, returning any outputs.
+let deployTemplate : IDeployment -> _ =
+    deployTemplateWithStatus
+    >> Seq.choose(function | DeploymentCompleted outputs -> Some outputs | DeploymentError _ | DeploymentInProgress _ -> None)
+    >> Seq.head
 
 /// Creates parameters from key/value string pairs used by the Fluent API.
 let buildArmParameters keyValues =
--- src/FsHelpers/JsonHelper/JsonHelper.fs
+++ src/FsHelpers/JsonHelper/JsonHelper.fs
@@ -10,11 +10,11 @@ and Json =
     | Object of ObjectData[]
     | Array of Json[]
     | Null
-    member this.AsString = match this with Json.String s -> s | _ -> failwith "Expected a JSON string"
-    member this.AsNumber = match this with Json.Number s -> s | _ -> failwith "Expected a JSON number"
-    member this.AsBoolean = match this with Json.Boolean b -> b | _ -> failwith "Expected a JSON boolean"
-    member this.AsObject = match this with Json.Object o -> o | _ -> failwith "Expected a JSON object"
-    member this.AsArray = match this with Json.Array o -> o | _ -> failwith "Expected a JSON array"
+    member this.AsString = match this with String s -> s | _ -> failwith "Expected a JSON string"
+    member this.AsNumber = match this with Number s -> s | _ -> failwith "Expected a JSON number"
+    member this.AsBoolean = match this with Boolean b -> b | _ -> failwith "Expected a JSON boolean"
+    member this.AsObject = match this with Object o -> o | _ -> failwith "Expected a JSON object"
+    member this.AsArray = match this with Array o -> o | _ -> failwith "Expected a JSON array"
     member this.TryGetProperty key =
         match this with
         | Json.Object data -> Some data
@@ -30,13 +30,13 @@ let rec ofJToken (jToken:JToken) =
     match jToken with
     | :? JValue as v ->
         match v.Value with
-        | :? string as s -> Json.String s
-        | :? decimal as d -> Json.Number (decimal d)
-        | :? int64 as i -> Json.Number (decimal i)
-        | :? int32 as i -> Json.Number (decimal i)
-        | :? float as f -> Json.Number (decimal f)
-        | :? bool as b -> Json.Boolean b
-        | null -> Json.Null
+        | :? string as s -> String s
+        | :? decimal as d -> Number (decimal d)
+        | :? int64 as i -> Number (decimal i)
+        | :? int32 as i -> Number (decimal i)
+        | :? float as f -> Number (decimal f)
+        | :? bool as b -> Boolean b
+        | null -> Null
         | v -> failwithf "Cannot convert JSON value type %s" (v.GetType().FullName)
     | :? JObject as o -> o.Properties() |> Seq.map (fun p -> p.Name, ofJToken p.Value) |> Seq.toArray |> Json.Object
     | :? JArray as a -> a.Values() |> Seq.map ofJToken |> Seq.toArray |> Json.Array
@@ -44,6 +44,6 @@ let rec ofJToken (jToken:JToken) =
 
 /// Match an Object and treat Null as an empty Object
 let (|ObjectOrNull|_|) = function
-    | Json.Object o -> Some o
-    | Json.Null -> Some [||]
+    | Object o -> Some o
+    | Null -> Some [||]
     | _ -> None
