fbeac4795558df17075e3974562428e8ed269f0e
Marnee Dearman <marneedearman@email.arizona.edu>
2018-12-30 11:20:18 -0700

    Issue #3: forgot to also swap the arguments

--- src/FsHelpers/ArmHelper/ArmHelper.fs
+++ src/FsHelpers/ArmHelper/ArmHelper.fs
@@ -109,7 +109,7 @@ open Microsoft.IdentityModel.Clients.ActiveDirectory
 
 /// Authenticates a device to Azure which either doesn't have a web browser or doesn't have a mechanism for user input. Examples of these
 /// include either terminals or IoT devices
-let authenticateDevice (showMessagePrompt:string -> unit) credentials (subscriptionId:Guid) =
+let authenticateDevice (credentials:AuthenticationCredentials) (subscriptionId:Guid) =
     let spi = AzureCredentialsFactory().FromServicePrincipal(string credentials.ClientId, credentials.ClientSecret, string credentials.TenantId, AzureEnvironment.AzureGlobalCloud)
     ResourceManager
         .Authenticate(spi)
@@ -117,7 +117,7 @@ let authenticateDevice (showMessagePrompt:string -> unit) credentials (subscript
         |> AuthenticatedContext
 
 /// Authenticates to Azure using the supplied credentials for a specific subscription. You will be promted to go through an authentication process.
-let authenticate (credentials:AuthenticationCredentials) (subscriptionId:Guid) =
+let authenticate (showMessagePrompt:string -> unit) credentials (subscriptionId:Guid) =
     let authority =
         sprintf "https://login.microsoftonline.com/%s" (match credentials.TenantId with Some tId -> tId.ToString() | None -> "common")
     let adalClient = AuthenticationContext(authority)
